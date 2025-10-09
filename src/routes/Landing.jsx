b9ec1d6ee2d3561228a3430893ac6dcf358ab3e3
Tristantantantan <gatbuntontristan@gmail.com>
2025-10-09 23:38:47 +0800

    changes

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -13,7 +13,7 @@ export const Landing = () => {
                         <h4 className='my-4'>Nature&apos;s Fury. A Volcanologist&apos;s Journey</h4>
                         <span>Venture through perilous regions in search of Mt. Thesaurus&apos; hidden treasure.</span>
                         <div className="mt-4">
-                            <a href="https://drive.google.com/file/d/1X2G4Ds-ao7ci1MEM556SxlxpMzUMYuTj/view" className="btn btn-primary">
+                            <a href="https://drive.google.com/file/d/18x5tA931gurLWnIEr3QBzEUj6a0o6uGy/view?usp=sharing" className="btn btn-primary">
                                 <i className="bi bi-download me-2"></i>
                                 Download Game
                             </a>
